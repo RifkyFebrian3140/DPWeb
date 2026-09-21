ca89ad3c5da28d94dd18a6f4c2786c796785b6c8
RifkyFebrian3140 <nightmon3140@gmail.com>
2026-09-21 12:33:45 +0700

    koneksi

--- Jobsheet 8/includes/koneksi.php
+++ Jobsheet 8/includes/koneksi.php
@@ -1,13 +1,26 @@
 <?php
-$host = "localhost";
+
+$host = "db.vgypxaibjupqvutmgymm.supabase.co";
 $port = "5432";
-$db   = "simpus_mini";
+$db = "postgres";
 $user = "postgres";
-$pass = "postgres";
+
+// Isi dengan password database Supabase yang sebenarnya
+$pass = "PASSWORD_DATABASE_KAMU";
 
 try {
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$db;sslmode=require",
+        $user,
+        $pass
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
 } catch (PDOException $e) {
     die("Koneksi database gagal: " . $e->getMessage());
 }
+?>
